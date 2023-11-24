cea888b7262bdb12d0cb2d56592491e6f374da90
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2023-11-24 23:43:50 +0900

    Event関数を作りdragStartの機能を入れる

--- main.js
+++ main.js
@@ -2,3 +2,7 @@
 const fill = document.querySelector(".fill");
 const empties = document.querySelectorAll(".empty");
 //console.log(empties);
+
+//2. ドラッグのトリガー
+fill.addEventListener("dragstart", dragStart);
+
